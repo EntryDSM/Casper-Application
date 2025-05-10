e91296747f1ba839d9b7c2b3c457988d5a88e992
박주원 <qkrwndnjs7308@gmail.com>
2025-05-10 14:38:33 +0900

    feat ( #2 ) : 커스텀 exception 추가 ( 그레들 플러그인은 global exception을 만들기 어려움 )

--- casper-convention/src/main/kotlin/io/casper/convention/tasks/DocCheckTask.kt
+++ casper-convention/src/main/kotlin/io/casper/convention/tasks/DocCheckTask.kt
@@ -1,9 +1,9 @@
 package io.casper.convention.tasks
 
+import io.casper.convention.exception.DocumentationException
 import io.casper.convention.model.CodeElement
 import io.casper.convention.service.DocCheckService
 import org.gradle.api.DefaultTask
-import org.gradle.api.GradleException
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
@@ -25,7 +25,7 @@ abstract class DocCheckTask : DefaultTask() {
      * 태스크 실행 시 호출되는 메소드입니다.
      * 문서화 검사를 수행하고 결과를 처리합니다.
      * 
-     * @throws GradleException 문서화 검사 실패 시 발생
+     * @throws DocumentationException 문서화 검사 실패 시 발생
      */
     @TaskAction
     fun check() {
@@ -35,10 +35,7 @@ abstract class DocCheckTask : DefaultTask() {
         val success = checkService.checkDocumentation(element)
         
         if (!success) {
-            throw GradleException(
-                "일부 ${element.friendlyName}에 KDoc 주석이 없습니다. " +
-                "자세한 내용은 로그를 확인하세요."
-            )
+            throw DocumentationException.missingDocumentation(element)
         }
     }
 }
