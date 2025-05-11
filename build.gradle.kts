ea05fc2c56f58ec32fbc4e1e4937578a17981ee9
박주원 <qkrwndnjs7308@gmail.com>
2025-05-11 20:19:51 +0900

    refactor ( #11 ) : 불필요한 루트 gradle.kts dependencies 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,6 @@ java {
     }
 }
 
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
