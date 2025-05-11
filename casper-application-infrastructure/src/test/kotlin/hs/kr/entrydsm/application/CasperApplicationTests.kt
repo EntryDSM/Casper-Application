e15cea938d158565c48dca2a91803982ee17aef3
박주원 <qkrwndnjs7308@gmail.com>
2025-05-11 20:36:14 +0900

    feat ( #11 ) : TestClass 추가

---
+++ casper-application-infrastructure/src/test/kotlin/hs/kr/entrydsm/application/CasperApplicationTests.kt
@@ -0,0 +1,12 @@
+package hs.kr.entrydsm.application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CasperApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
