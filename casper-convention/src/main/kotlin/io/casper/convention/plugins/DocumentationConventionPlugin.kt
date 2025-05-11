2a5942f7b508223484f0f253f64144df3420197d
박주원 <qkrwndnjs7308@gmail.com>
2025-05-11 18:53:28 +0900

    refactor ( #11 ) : 하드 코딩 제거 및 함수명 명확하게 변경

--- casper-convention/src/main/kotlin/io/casper/convention/plugins/DocumentationConventionPlugin.kt
+++ casper-convention/src/main/kotlin/io/casper/convention/plugins/DocumentationConventionPlugin.kt
@@ -15,11 +15,11 @@ import org.gradle.kotlin.dsl.register
 class DocumentationConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.logger.lifecycle("문서화 규칙 플러그인을 적용합니다: ${project.name}")
-        registerDocTasks(project)
+        registerDocCheckTasks(project)
     }
 
 
-    private fun registerDocTasks(project: Project) {
+    private fun registerDocCheckTasks(project: Project) {
         val registeredTasks = mutableListOf<TaskProvider<out Task>>()
 
         DocCheckTaskType.values().forEach { taskType ->
@@ -41,23 +41,25 @@ class DocumentationConventionPlugin : Plugin<Project> {
         }
 
         // 모든 문서화 검사를 한 번에 실행하는 태스크
-        val checkAllDocsTaskName = "checkAllDocs"
-        if (project.tasks.findByName(checkAllDocsTaskName) == null) {
-            project.tasks.register(checkAllDocsTaskName) {
+        if (project.tasks.findByName(CHECK_ALL_DOCS_TASK) == null) {
+            project.tasks.register(CHECK_ALL_DOCS_TASK) {
                 group = DocConstants.CHECK_GROUP
                 description = "모든 코드 요소의 KDoc 주석 여부를 확인합니다"
                 
                 // 등록된 모든 검사 태스크에 의존
                 dependsOn(registeredTasks)
             }
-            project.logger.lifecycle("${project.name}에 ${checkAllDocsTaskName} 태스크를 등록했습니다.")
+            project.logger.lifecycle("${project.name}에 ${CHECK_ALL_DOCS_TASK} 태스크를 등록했습니다.")
         }
         
         // 프로젝트 평가 후에 check 태스크가 있으면 checkAllDocs를 의존성으로 추가
         project.afterEvaluate {
-            project.tasks.findByName("check")?.dependsOn(checkAllDocsTaskName)
-            project.logger.lifecycle("${project.name}의 check 태스크에 ${checkAllDocsTaskName} 의존성을 추가했습니다.")
+            project.tasks.findByName("check")?.dependsOn(CHECK_ALL_DOCS_TASK)
+            project.logger.lifecycle("${project.name}의 check 태스크에 ${CHECK_ALL_DOCS_TASK} 의존성을 추가했습니다.")
         }
 
+    }
+    companion object {
+        const val CHECK_ALL_DOCS_TASK = "checkAllDocs"
     }
 }
