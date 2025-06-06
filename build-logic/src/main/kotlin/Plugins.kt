8ceb12d701619a6a382b0abe833213a99fe7a3ed
kangeunchan <kangeunchan080310@gmail.com>
2025-06-06 16:50:12 +0900

    feat ( #18 ) : Plugin Dependencies 추가

--- build-logic/src/main/kotlin/Plugins.kt
+++ build-logic/src/main/kotlin/Plugins.kt
@@ -1,2 +1,7 @@
 object Plugins {
+    const val KOTLIN_JVM = "jvm"
+    const val KOTLIN_SPRING = "plugin.spring"
+    const val SPRING_BOOT = "org.springframework.boot"
+    const val SPRING_DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
+    const val KTLINT = "org.jlleitschuh.gradle.ktlint"
 }
