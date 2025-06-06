8f654f125d9d94480071d0e80f7abdfbfd94e8b2
kangeunchan <kangeunchan080310@gmail.com>
2025-06-06 16:53:34 +0900

    feat ( #18 ) : Dependencies 추가

--- build-logic/src/main/kotlin/Dependencies.kt
+++ build-logic/src/main/kotlin/Dependencies.kt
@@ -1,2 +1,8 @@
 object Dependencies {
+    const val SPRING_BOOT_STARTER = "org.springframework.boot:spring-boot-starter"
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+
+    const val SPRING_BOOT_STARTER_TEST = "org.springframework.boot:spring-boot-starter-test"
+    const val KOTLIN_TEST_JUNIT5 = "org.jetbrains.kotlin:kotlin-test-junit5"
+    const val JUNIT_PLATFORM_LAUNCHER = "org.junit.platform:junit-platform-launcher"
 }
