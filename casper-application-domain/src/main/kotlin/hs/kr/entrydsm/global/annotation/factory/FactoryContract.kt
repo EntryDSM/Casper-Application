a168a074b19b0fa14c2a9a8d2d4d4fe40ee43684
kangeunchan <kangeunchan080310@gmail.com>
2025-07-10 17:07:29 +0900

    feat ( #21 ) : FactoryContract 인터페이스 메서드 getContext 및 getTargetType 추가

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/global/annotation/factory/FactoryContract.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/global/annotation/factory/FactoryContract.kt
@@ -2,4 +2,6 @@ package hs.kr.entrydsm.global.annotation.factory
 
 interface FactoryContract<T> {
     fun create(vararg params: Any?): T
+    fun getContext(): String
+    fun getTargetType(): Class<T>
 }
