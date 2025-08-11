0088fafe8e2d842ad0cc591c57838f6d45380a64
coehgns <modooboiroo@gmail.com>
2025-08-11 15:46:14 +0900

    feat ( #21 ) : DomainEvents object 추가

---
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/domain/ast/events/DomainEvents.kt
@@ -0,0 +1,10 @@
+package hs.kr.entrydsm.domain.ast.events
+
+object DomainEvents {
+    const val AST_CREATED = "AST_CREATED"
+    const val AST_MODIFIED = "AST_MODIFIED"
+    const val SUBTREE_REPLACED = "SUBTREE_REPLACED"
+    const val EXPRESSION_AST = "ExpressionAST"
+    const val AST_OPTIMIZED = "AST_OPTIMIZED"
+    const val AST_VALIDATED = "AST_VALIDATED"
+}
