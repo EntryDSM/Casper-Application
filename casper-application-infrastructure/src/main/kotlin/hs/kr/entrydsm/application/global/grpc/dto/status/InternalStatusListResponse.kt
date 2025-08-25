57b973143eea2e45ccdd787d74c99a0c9797369c
박주원 <qkrwndnjs7308@gmail.com>
2025-08-25 19:49:40 +0900

    feat ( #27 ) : dto 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/grpc/dto/status/InternalStatusListResponse.kt
@@ -0,0 +1,11 @@
+package hs.kr.entrydsm.application.global.grpc.dto.status
+
+/**
+ * 내부 gRPC 통신용 상태 리스트 응답 DTO 클래스입니다.
+ * 상태 서비스와의 통신에서 전체 상태 정보 리스트를 전달받는 데 사용됩니다.
+ *
+ * @property statusList 상태 정보 리스트
+ */
+data class InternalStatusListResponse(
+    val statusList: List<InternalStatusResponse>
+)
