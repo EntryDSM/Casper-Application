d856fd4b7472a993623d37e3222ffb7b301d99de
coehgns <modooboiroo@gmail.com>
2025-08-25 20:48:44 +0900

    refactor ( #23 ) : StatusGrpcClient에 맞게 StatusPersistenceAdapter 수정

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/status/StatusPersistenceAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/status/StatusPersistenceAdapter.kt
@@ -1,7 +1,6 @@
 package hs.kr.entrydsm.application.domain.status
 
 import hs.kr.entrydsm.application.global.grpc.client.status.StatusGrpcClient
-import hs.kr.entrydsm.application.global.grpc.client.status.dto.request.InternalUpdateExamCodeRequest
 import hs.kr.entrydsm.domain.status.interfaces.StatusContract
 import org.springframework.stereotype.Component
 
@@ -13,7 +12,6 @@ class StatusPersistenceAdapter(
         receiptCode: Long,
         examCode: String,
     ) {
-        val response = InternalUpdateExamCodeRequest(receiptCode, examCode)
-        statusGrpcClient.updateExamCode(response)
+        statusGrpcClient.updateExamCode(receiptCode, examCode)
     }
 }
