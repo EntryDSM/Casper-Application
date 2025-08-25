b149b1e16ea80df18b7688cf9c401797cfd19739
coehgns <modooboiroo@gmail.com>
2025-08-25 23:36:32 +0900

    docs ( #27 ) : InternalScheduleResponse에 kdoc 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/grpc/dto/schedule/InternalScheduleResponse.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/grpc/dto/schedule/InternalScheduleResponse.kt
@@ -2,6 +2,11 @@ package hs.kr.entrydsm.application.global.grpc.dto.schedule
 
 import java.time.LocalDateTime
 
+/**
+ * gRPC를 통해 다른 서비스에서 받아온 일정 정보를 담는 데이터 클래스입니다.
+ * @property type 일정의 종류
+ * @property date 일정의 날짜와 시간
+ */
 data class InternalScheduleResponse(
     val type: ScheduleType,
     val date: LocalDateTime,
