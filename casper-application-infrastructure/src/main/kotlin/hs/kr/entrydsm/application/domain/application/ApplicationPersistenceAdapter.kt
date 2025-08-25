f9088ba5f21e0dbdb6e6ad86762235f2516f0485
coehgns <modooboiroo@gmail.com>
2025-08-25 20:48:55 +0900

    refactor ( #23 ) : StatusGrpcClient에 맞게 ApplicationPersistenceAdapter 수정

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/ApplicationPersistenceAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/ApplicationPersistenceAdapter.kt
@@ -4,7 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 import hs.kr.entrydsm.application.domain.application.entity.QApplicationJpaEntity.applicationJpaEntity
 import hs.kr.entrydsm.application.domain.application.mapper.ApplicationMapper
 import hs.kr.entrydsm.application.global.grpc.client.status.StatusGrpcClient
-import hs.kr.entrydsm.application.global.grpc.client.status.dto.response.StatusInfoElement
+import hs.kr.entrydsm.application.global.grpc.dto.status.InternalStatusResponse
 import hs.kr.entrydsm.domain.application.aggregates.Application
 import hs.kr.entrydsm.domain.application.interfaces.ApplicationContract
 import org.springframework.stereotype.Component
@@ -19,7 +19,7 @@ class ApplicationPersistenceAdapter(
         val firstRoundPassStatusKeyList =
             statusGrpcClient.getStatusList().statusList
                 .filter { it.isFirstRoundPass }
-                .associateBy(StatusInfoElement::receiptCode)
+                .associateBy(InternalStatusResponse::receiptCode)
                 .keys.toList()
 
         return jpaQueryFactory
