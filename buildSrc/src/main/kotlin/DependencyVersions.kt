27e31e8f72c196b9987e7c3608e4103d86d08fdb
coehgns <modooboiroo@gmail.com>
2025-08-25 17:22:17 +0900

    feat ( #23 ) : DependencyVersions

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -7,4 +7,18 @@ object DependencyVersions {
     
     // Kotlinx Coroutines
     const val KOTLINX_COROUTINES_VERSION = "1.8.1"
+
+    const val GRPC = "1.61.1"
+    const val GRPC_KOTLIN = "1.4.1"
+    const val PROTOBUF = "3.25.3"
+    const val GRPC_CLIENT = "2.15.0.RELEASE"
+    const val GOOGLE_PROTOBUF = "3.25.3"
+
+    const val COROUTINES = "1.8.0"
+    const val MAPSTRUCT = "1.6.0"
+
+    const val QUERYDSL = "5.0.0"
+    const val JAKARTA_PERSISTENCE = "3.1.0"
+    const val JAKARTA_ANNOTATION = "2.1.1"
+    const val CAFFEINE = "3.1.8"
 }
