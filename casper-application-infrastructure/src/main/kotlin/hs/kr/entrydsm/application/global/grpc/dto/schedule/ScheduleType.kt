21a012bed7f8c71844caa72e934c8005b61c6bab
coehgns <modooboiroo@gmail.com>
2025-08-25 23:31:25 +0900

    feat ( #27 ) : ScheduleType 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/grpc/dto/schedule/ScheduleType.kt
@@ -0,0 +1,9 @@
+package hs.kr.entrydsm.application.global.grpc.dto.schedule
+
+enum class ScheduleType {
+    START_DATE,
+    FIRST_ANNOUNCEMENT,
+    INTERVIEW,
+    SECOND_ANNOUNCEMENT,
+    END_DATE,
+}
