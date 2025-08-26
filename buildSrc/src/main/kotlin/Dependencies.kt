d959f40e02950fd567b60a3ed66a338de4c55911
coehgns <modooboiroo@gmail.com>
2025-08-26 17:07:16 +0900

    fix ( #23 ) : conflict

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,40 +1,38 @@
 object Dependencies {
-    // kotlin
+    //kotlin
     const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
     const val KOTLIN_TEST = "org.jetbrains.kotlin:kotlin-test"
 
-    // springframework
+    //springframework
     const val SPRING_BOOT_STARTER = "org.springframework.boot:spring-boot-starter"
     const val SPRING_BOOT_STARTER_WEB = "org.springframework.boot:spring-boot-starter-web"
     const val SPRING_BOOT_STARTER_TEST = "org.springframework.boot:spring-boot-starter-test"
     const val SPRING_BOOT_STARTER_ACTUATOR  = "org.springframework.boot:spring-boot-starter-actuator"
-    const val SPRING_BOOT_STARTER_DATA_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
-    const val THYMELEAF = "org.springframework.boot:spring-boot-starter-thymeleaf"
 
-    // jexl
+    //jexl
     const val APACHE_COMMONS_JEXL = "org.apache.commons:commons-jexl3:${DependencyVersions.APACHE_COMMONS_JEXL_VERSION}"
 
-    // kotlinx serialization
+    //kotlinx serialization
     const val KOTLINX_SERIALIZATION_JSON = "org.jetbrains.kotlinx:kotlinx-serialization-json:${DependencyVersions.KOTLINX_SERIALIZATION_VERSION}"
 
-    // kotlinx coroutines
+    //kotlinx coroutines
     const val KOTLINX_COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DependencyVersions.KOTLINX_COROUTINES_VERSION}"
-    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DependencyVersions.COROUTINES}"
-    const val COROUTINES_REACTOR = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor"
 
-    // junit
+    //junit
     const val JUNIT = "org.jetbrains.kotlin:kotlin-test-junit5"
     const val JUNIT_PLATFORM_LAUNCHER = "org.junit.platform:junit-platform-launcher"
 
-    // poi
+    //poi
     const val POI = "org.apache.poi:poi:${DependencyVersions.POI_VERSION}"
     const val POI_OOXML = "org.apache.poi:poi-ooxml:${DependencyVersions.POI_VERSION}"
 
-    // Pdf
+    //Pdf
     const val PDF_ITEXT = "com.itextpdf:itext7-fonts:${DependencyVersions.PDF_ITEXT}"
     const val PDF_HTML = "com.itextpdf:html2pdf:${DependencyVersions.PDF_HTML}"
 
-    // commons io
+    const val THYMELEAF = "org.springframework.boot:spring-boot-starter-thymeleaf"
+
+    //commons io
     const val COMMONS_IO = "commons-io:commons-io:${DependencyVersions.COMMONS_IO}"
 
     // WebFlux
@@ -49,6 +47,9 @@ object Dependencies {
     const val GRPC_TESTING = "io.grpc:grpc-testing:${DependencyVersions.GRPC}"
     const val GRPC_CLIENT = "net.devh:grpc-client-spring-boot-starter:${DependencyVersions.GRPC_CLIENT}"
     const val GOOGLE_PROTOBUF = "com.google.protobuf:protobuf-java:${DependencyVersions.GOOGLE_PROTOBUF}"
+
+    // Coroutines
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DependencyVersions.COROUTINES}"
 
     // MapStruct
     const val MAPSTRUCT = "org.mapstruct:mapstruct:${DependencyVersions.MAPSTRUCT}"
@@ -71,6 +72,12 @@ object Dependencies {
     // Jackson Kotlin module
     const val JACKSON_MODULE_KOTLIN = "com.fasterxml.jackson.module:jackson-module-kotlin"
 
+    // Coroutines Reactor bridge
+    const val COROUTINES_REACTOR = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor"
+
     // Reactor Netty
     const val REACTOR_NETTY = "io.projectreactor.netty:reactor-netty"
+
+    // JPA
+    const val SPRING_BOOT_STARTER_DATA_JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
 }
