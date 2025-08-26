b9d815cb54c16d723ab2d7a0c3a126e0a890ba46
coehgns <modooboiroo@gmail.com>
2025-08-26 18:29:26 +0900

    feat ( #23 ) : GlobalComponentScan

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/aop/GlobalComponentScan.kt
@@ -0,0 +1,21 @@
+package hs.kr.entrydsm.application.global.aop
+
+import hs.kr.entrydsm.global.annotation.usecase.UseCase
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.ComponentScan.Filter
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+
+@Configuration
+@ComponentScan(
+    basePackages = ["hs.kr.entrydsm.application"],
+    includeFilters = [
+        Filter(
+            type = FilterType.ANNOTATION,
+            classes = [
+                UseCase::class
+            ],
+        ),
+    ],
+)
+class GlobalComponentScan
