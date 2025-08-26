c7e57102a94c9c010006b28e1cc35563b4717c5e
coehgns <modooboiroo@gmail.com>
2025-08-26 17:51:14 +0900

    docs ( #23 ) : PropertiesConfig kdoc 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/PropertiesConfig.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/PropertiesConfig.kt
@@ -1,9 +1,15 @@
 package hs.kr.entrydsm.application.global.config
 
-import hs.kr.entrydsm.application.domain.examcode.KakaoBaseProperties
+import hs.kr.entrydsm.application.domain.examcode.KakaoProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
+/**
+ * `@ConfigurationProperties` 어노테이션이 붙은 클래스들을 스캔하기 위한 Configuration 입니다.
+ *
+ * @author chaedohun
+ * @since 2025.08.26
+ */
 @Configuration
-@EnableConfigurationProperties(KakaoBaseProperties::class)
+@EnableConfigurationProperties(KakaoProperties::class)
 class PropertiesConfig
