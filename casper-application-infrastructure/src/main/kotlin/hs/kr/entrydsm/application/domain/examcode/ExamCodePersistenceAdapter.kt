a9d49712ac79f48ed86874d47d85e3c1d436ab10
coehgns <modooboiroo@gmail.com>
2025-08-26 17:50:44 +0900

    docs ( #23 ) : ExamCodePersistenceAdapter kdoc 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/ExamCodePersistenceAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/ExamCodePersistenceAdapter.kt
@@ -3,11 +3,26 @@ package hs.kr.entrydsm.application.domain.examcode
 import hs.kr.entrydsm.domain.examcode.interfaces.BaseLocationContract
 import org.springframework.stereotype.Component
 
+/**
+ * 학교의 위치 정보를 외부(properties)에서 가져오는 Persistence Adapter 입니다.
+ *
+ * @author chaedohun
+ * @since 2025.08.26
+ */
 @Component
 class ExamCodePersistenceAdapter(
-    private val kakaoBaseProperties: KakaoBaseProperties,
+    private val kakaoProperties: KakaoProperties,
 ) : BaseLocationContract {
-    override val baseLat: Double get() = kakaoBaseProperties.lat
 
-    override val baseLon: Double get() = kakaoBaseProperties.lon
+    /**
+     * 기준이 되는 장소의 위도입니다.
+     * @see kakaoProperties.lat
+     */
+    override val baseLat: Double get() = kakaoProperties.lat
+
+    /**
+     * 기준이 되는 장소의 경도입니다.
+     * @see kakaoProperties.lon
+     */
+    override val baseLon: Double get() = kakaoProperties.lon
 }
