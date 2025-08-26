d68ed0ef954eb8cbba4df74fa020b421e35cc499
coehgns <modooboiroo@gmail.com>
2025-08-26 14:44:30 +0900

    docs ( #23 ) : BaseLocationContract kdoc 추가

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/domain/examcode/interfaces/BaseLocationContract.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/domain/examcode/interfaces/BaseLocationContract.kt
@@ -1,6 +1,19 @@
 package hs.kr.entrydsm.domain.examcode.interfaces
 
+/**
+ * 거리 계산의 기준이 되는 위치 정보에 대한 인터페이스입니다.
+ *
+ * @author chaedohun
+ * @since 2025.08.26
+ */
 interface BaseLocationContract {
+    /**
+     * 기준 위도
+     */
     val baseLat: Double
+
+    /**
+     * 기준 경도
+     */
     val baseLon: Double
 }
