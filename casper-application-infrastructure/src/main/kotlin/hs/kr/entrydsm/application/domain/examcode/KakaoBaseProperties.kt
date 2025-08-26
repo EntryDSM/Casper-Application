1743f5ae9e670a28c152f60aed858a7049c0a715
coehgns <modooboiroo@gmail.com>
2025-08-26 17:51:29 +0900

    refactor ( #23 ) : KakaoBaseProperties -> KakaoProperties

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/KakaoBaseProperties.kt
+++
@@ -1,9 +0,0 @@
-package hs.kr.entrydsm.application.domain.examcode
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "kakao.base")
-class KakaoBaseProperties(
-    val lat: Double,
-    val lon: Double,
-)
