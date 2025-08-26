d0ebced8741c501f9048e23a3d30a70bb49e56a9
coehgns <modooboiroo@gmail.com>
2025-08-26 17:50:29 +0900

    docs ( #23 ) : WebExamCodeAdapter kdoc 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/presentation/WebExamCodeAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/presentation/WebExamCodeAdapter.kt
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * 수험번호 부여에 대한 요청을 외부로부터 받는 Web Adapter 입니다.
+ *
+ * @author chaedohun
+ * @since 2025.08.26
+ */
 @RestController
 @RequestMapping("/exam-code")
 class WebExamCodeAdapter(
     private val grantExamCodesContract: GrantExamCodesContract,
 ) {
+
+    /**
+     * `POST /exam-code`
+     * 수험번호를 일괄적으로 부여합니다.
+     */
     @PostMapping
     suspend fun grantExamCodes() = grantExamCodesContract.execute()
 }
