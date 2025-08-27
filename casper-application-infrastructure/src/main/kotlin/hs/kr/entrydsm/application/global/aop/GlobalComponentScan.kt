15b78b7ce0d0640af10780517c5c864c1239fc1e
coehgns <modooboiroo@gmail.com>
2025-08-27 16:39:49 +0900

    docs ( #23 ) : GlobalComponentScan에 kdoc 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/aop/GlobalComponentScan.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/aop/GlobalComponentScan.kt
@@ -6,6 +6,16 @@ import org.springframework.context.annotation.ComponentScan.Filter
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.FilterType
 
+/**
+ * 애플리케이션 전역에서 특정 어노테이션을 스캔하여 컴포넌트로 등록하는 설정을 담당하는 클래스입니다.
+ * [UseCase] 어노테이션이 적용된 클래스를 찾아 스프링 컨텍스트에 빈으로 등록합니다.
+ *
+ * @see UseCase
+ * @see ComponentScan
+ *
+ * @author chaedohun
+ * @since 2025.08.27
+ */
 @Configuration
 @ComponentScan(
     basePackages = ["hs.kr.entrydsm.application"],
