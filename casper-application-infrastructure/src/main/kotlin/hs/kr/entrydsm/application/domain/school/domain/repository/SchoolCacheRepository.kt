05b4f2c983a76a2f3fddc9666a8fd11616a6dccb
박주원 <qkrwndnjs7308@gmail.com>
2025-08-27 17:19:47 +0900

    feat ( #28 ) : redis Repository 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/school/domain/repository/SchoolCacheRepository.kt
@@ -0,0 +1,6 @@
+package hs.kr.entrydsm.application.domain.school.domain.repository
+
+import hs.kr.entrydsm.application.domain.school.domain.entity.SchoolCacheRedisEntity
+import org.springframework.data.repository.CrudRepository
+
+interface SchoolCacheRepository : CrudRepository<SchoolCacheRedisEntity, String>
