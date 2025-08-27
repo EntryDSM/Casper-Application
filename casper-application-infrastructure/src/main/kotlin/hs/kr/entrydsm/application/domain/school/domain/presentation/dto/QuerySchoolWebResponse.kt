197882e9451d3b482e9209fea8464c2021de9ca7
박주원 <qkrwndnjs7308@gmail.com>
2025-08-27 17:18:45 +0900

    feat ( #28 ) : web dto 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/school/domain/presentation/dto/QuerySchoolWebResponse.kt
@@ -0,0 +1,5 @@
+package hs.kr.entrydsm.application.domain.school.domain.presentation.dto
+
+data class QuerySchoolWebResponse(
+    val content: List<SchoolWebResponse>? = null,
+)
---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/school/domain/presentation/dto/SchoolWebResponse.kt
@@ -0,0 +1,8 @@
+package hs.kr.entrydsm.application.domain.school.domain.presentation.dto
+
+data class SchoolWebResponse(
+    val code: String? = null,
+    val name: String? = null,
+    val information: String? = null,
+    val address: String? = null,
+)
