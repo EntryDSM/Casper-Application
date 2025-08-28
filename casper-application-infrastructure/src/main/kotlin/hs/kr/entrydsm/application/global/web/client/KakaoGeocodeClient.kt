dc37c9fa4208ef5226a0709880bfbe5e98cd5bd9
coehgns <modooboiroo@gmail.com>
2025-08-28 14:34:21 +0900

    refactor ( #32 ) : KakaoGeocodeClient 컴포넌트 어노테이션으로 수정

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/web/client/KakaoGeocodeClient.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/web/client/KakaoGeocodeClient.kt
@@ -6,6 +6,7 @@ import hs.kr.entrydsm.global.annotation.service.Service
 import hs.kr.entrydsm.global.annotation.service.type.ServiceType
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
@@ -15,7 +16,7 @@ import org.springframework.web.reactive.function.client.bodyToMono
  * @author chaedohun
  * @since 2025.08.26
  */
-@Service(name = "KakaoGeocodeClient", type = ServiceType.APPLICATION_SERVICE)
+@Component
 class KakaoGeocodeClient(
     private val builder: WebClient.Builder,
     private val kakaoBaseProperties: KakaoProperties,
