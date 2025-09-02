b10dea715f017e033cfa73f027a0782e3c178fdd
박주원 <qkrwndnjs7308@gmail.com>
2025-09-02 17:51:12 +0900

    feat ( #36 ) : RedisEntity 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/status/entity/StatusCacheRedisEntity.kt
@@ -0,0 +1,17 @@
+package hs.kr.entrydsm.application.domain.status.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+
+@RedisHash("status_cache")
+class StatusCacheRedisEntity(
+    @Id
+    val receiptCode: Long,
+    val examCode: String?,
+    val applicationStatus: ApplicationStatus,
+    val isFirstRoundPass: Boolean,
+    val isSecondRoundPass: Boolean,
+    @TimeToLive
+    val ttl: Long
+)
