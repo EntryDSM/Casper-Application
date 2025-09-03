2139148aa7270db6aadde2dc288e22bd63fa799a
박주원 <qkrwndnjs7308@gmail.com>
2025-09-03 19:55:52 +0900

    feat ( #33 ) : kafkaTopics 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/kafka/configuration/KafkaTopics.kt
@@ -0,0 +1,8 @@
+package hs.kr.entrydsm.application.global.kafka.configuration
+
+object KafkaTopics {
+    const val CREATE_APPLICATION = "create-application"
+    const val DELETE_USER = "delete-user"
+    const val USER_RECEIPT_CODE_UPDATE_COMPLETED = "user-receipt-code-update-completed"
+    const val USER_RECEIPT_CODE_UPDATE_FAILED = "user-receipt-code-update-failed"
+}
