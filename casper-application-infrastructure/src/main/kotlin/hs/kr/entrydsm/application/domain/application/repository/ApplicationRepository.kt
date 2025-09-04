2d283d613ab6d440a9d4c1de4e26810e9a760408
coehgns <modooboiroo@gmail.com>
2025-09-04 18:52:39 +0900

    refactor ( #37 ) : ApplicationJpaRepository

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/repository/ApplicationRepository.kt
+++
@@ -1,6 +0,0 @@
-package hs.kr.entrydsm.application.domain.application.repository
-
-import hs.kr.entrydsm.application.domain.application.entity.ApplicationJpaEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ApplicationRepository : JpaRepository<ApplicationJpaEntity, Long>
