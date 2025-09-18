f7e5e71467cac56386957409cacd7ad8d3ad872b
박주원 <qkrwndnjs7308@gmail.com>
2025-09-18 18:56:10 +0900

    feat ( #49 ) : FilterConfig 추가

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/security/FilterConfig.kt
@@ -0,0 +1,23 @@
+package hs.kr.entrydsm.application.global.security
+
+import hs.kr.entrydsm.application.global.security.jwt.JwtFilter
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
+
+/**
+ * 시큐리티 필터 체인 설정을 담당하는 클래스입니다.
+ * JWT 필터를 Spring Security 필터 체인에 등록합니다.
+ */
+@Component
+class FilterConfig : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+    
+    override fun configure(builder: HttpSecurity) {
+        builder.addFilterBefore(
+            JwtFilter(),
+            UsernamePasswordAuthenticationFilter::class.java,
+        )
+    }
+}
