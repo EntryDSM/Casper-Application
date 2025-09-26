3811e3ab7fcf2a9f4ba6479f3823927dd024702f
coehgns <modooboiroo@gmail.com>
2025-09-27 00:23:04 +0900

    refactor ( #53 ) : AwsS3Config

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/AwsS3Config.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/AwsS3Config.kt
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import hs.kr.entrydsm.application.global.storage.AwsCredentialsProperties
 import hs.kr.entrydsm.application.global.storage.AwsProperties
+import hs.kr.entrydsm.application.global.storage.AwsRegionProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,8 +14,8 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @EnableConfigurationProperties(AwsProperties::class, AwsCredentialsProperties::class)
 class AwsS3Config(
-    private val awsProperties: AwsProperties,
-    private val awsCredentialsProperties: AwsCredentialsProperties
+    private val awsCredentialsProperties: AwsCredentialsProperties,
+    private val awsRegionProperties: AwsRegionProperties
 ) {
 
     @Bean
@@ -22,7 +23,7 @@ class AwsS3Config(
         val credentials = BasicAWSCredentials(awsCredentialsProperties.accessKey, awsCredentialsProperties.secretKey)
 
         return AmazonS3ClientBuilder.standard()
-            .withRegion(awsProperties.region)
+            .withRegion(awsRegionProperties.static)
             .withCredentials(AWSStaticCredentialsProvider(credentials))
             .build() as AmazonS3Client
     }
