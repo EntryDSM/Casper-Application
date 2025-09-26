242ed5edd88fcc33c0a104aa73a1057b183e2848
coehgns <modooboiroo@gmail.com>
2025-09-26 22:38:13 +0900

    feat ( #53 ) : AwsProperties

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/storage/AwsProperties.kt
@@ -0,0 +1,9 @@
+package hs.kr.entrydsm.application.global.storage
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("cloud.aws.s3")
+class AwsProperties(
+    val bucket: String,
+    val region: String,
+)
