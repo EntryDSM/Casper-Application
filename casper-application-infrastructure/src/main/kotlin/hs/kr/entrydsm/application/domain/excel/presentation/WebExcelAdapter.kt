3327d27394cf48d5772cef5f7955ce5362e38a3a
coehgns <modooboiroo@gmail.com>
2025-10-08 00:13:28 +0900

    chore : WebExcelAdapter RequestMapping 경로 수정

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/excel/presentation/WebExcelAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/excel/presentation/WebExcelAdapter.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/admin/excel")
+@RequestMapping("/api/v1/admin/excel")
 class WebExcelAdapter(
     private val printAdmissionTicketUseCase: PrintAdmissionTicketUseCase,
     private val printApplicantCodesUseCase: PrintApplicantCodesUseCase,
