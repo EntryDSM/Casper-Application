6b4803c46cfd54579e075c5e5a81e6196350d399
박주원 <qkrwndnjs7308@gmail.com>
2025-10-10 21:50:17 +0900

    refactor : 예외처리 추가

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/excel/usecase/PrintApplicantCodesUseCase.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/excel/usecase/PrintApplicantCodesUseCase.kt
@@ -14,9 +14,15 @@ class PrintApplicantCodesUseCase(
 ) {
     suspend fun execute(httpServletResponse: HttpServletResponse) {
         val applications = queryAllFirstRoundPassedApplicationContract.queryAllFirstRoundPassedApplication()
+
+        if (applications.isEmpty()) {
+            throw IllegalStateException("1차 합격자가 없습니다.")
+        }
+
         val receiptCodes = applications.map { it.receiptCode }
         val statuses = applicationQueryStatusContract.queryStatusesByReceiptCodes(receiptCodes)
 
         printApplicantCodesGenerator.execute(httpServletResponse, applications, statuses)
     }
+
 }
