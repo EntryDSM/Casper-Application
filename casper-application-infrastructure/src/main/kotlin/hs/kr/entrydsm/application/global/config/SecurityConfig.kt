354bd238a2dd19c5b28beed3a462fbc38d3c4843
coehgns <modooboiroo@gmail.com>
2025-10-13 19:12:44 +0900

    refactor : SecurityConfig

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/SecurityConfig.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/SecurityConfig.kt
@@ -57,7 +57,7 @@ class SecurityConfig(
                     .requestMatchers(HttpMethod.GET, "/admin/excel/**").hasRole(UserRole.ADMIN.name)
                     .requestMatchers(HttpMethod.GET, "/api/v1/application/pdf/**").hasRole(UserRole.USER.name)
                     .requestMatchers(HttpMethod.POST, "/api/v1/application/pdf/**").hasRole(UserRole.USER.name)
-                    .requestMatchers(HttpMethod.GET, "/api/v1/**").hasRole(UserRole.ADMIN.name)
+                    .requestMatchers(HttpMethod.GET, "/api/v1/admin/excel/**").hasRole(UserRole.ADMIN.name)
                     .requestMatchers("/api/v1/applications/**").hasRole(UserRole.USER.name)
                     .requestMatchers("/photo").hasRole(UserRole.USER.name)
                     .requestMatchers("/pass/**").hasRole(UserRole.USER.name)
