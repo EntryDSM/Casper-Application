a64a1b8fb1b0d53c613f8ff81df1daefb7b0aa2a
coehgns <modooboiroo@gmail.com>
2025-10-14 19:01:03 +0900

    chore : 수험번호 업데이트 엔드포인트 수정

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/presentation/WebExamCodeAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/examcode/presentation/WebExamCodeAdapter.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
  * @since 2025.08.26
  */
 @RestController
-@RequestMapping("/api/v1/exam-code")
+@RequestMapping("/exam-code")
 class WebExamCodeAdapter(
     private val grantExamCodesContract: GrantExamCodesContract,
 ) : ExamCodeApiDocument {
