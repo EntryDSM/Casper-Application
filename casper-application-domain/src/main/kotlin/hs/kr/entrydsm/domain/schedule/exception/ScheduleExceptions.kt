d628ad91c047939ebfb5d90ac1969439457b55d8
coehgns <modooboiroo@gmail.com>
2025-10-17 19:29:45 +0900

    refactor : ScheduleExceptions

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/domain/schedule/exception/ScheduleExceptions.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/domain/schedule/exception/ScheduleExceptions.kt
@@ -7,14 +7,14 @@ sealed class ScheduleExceptions(
     override val message: String,
 ) : BusinessException(status, message) {
 
-    class ScoreNotFoundException(message: String = SCORE_NOT_FOUND_EXCEPTION):
+    class ScheduleNotFoundException(message: String = SCHEDULE_NOT_FOUND_EXCEPTION):
         ScheduleExceptions(404, message)
 
     class AdmissionUnavailableException(message: String = ADMISSION_UNAVAILABLE):
         ScheduleExceptions(404, message)
 
     companion object {
-        private const val SCORE_NOT_FOUND_EXCEPTION = "점수가 존재하지 않습니다"
+        private const val SCHEDULE_NOT_FOUND_EXCEPTION = "일정이 존재하지 않습니다"
         private const val ADMISSION_UNAVAILABLE = "합격여부를 확인할 수 없습니다"
     }
 }
