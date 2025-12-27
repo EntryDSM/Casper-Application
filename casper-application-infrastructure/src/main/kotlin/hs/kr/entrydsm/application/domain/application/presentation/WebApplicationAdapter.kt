170167d2f097517067a533484bf75d389eaecb72
coehgns <modooboiroo@gmail.com>
2025-12-27 23:00:07 +0900

    chore ( #74 ) : 현재 서비스에서 사용되지 않는 application api 삭제

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/WebApplicationAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/WebApplicationAdapter.kt
@@ -1,34 +1,18 @@
 package hs.kr.entrydsm.application.domain.application.presentation
 
-import hs.kr.entrydsm.application.domain.application.presentation.dto.request.*
 import hs.kr.entrydsm.application.domain.application.usecase.*
-import hs.kr.entrydsm.application.domain.application.usecase.dto.request.*
 import hs.kr.entrydsm.application.domain.application.usecase.dto.response.*
 import hs.kr.entrydsm.application.domain.file.presentation.converter.ImageFileConverter
 import hs.kr.entrydsm.application.domain.file.presentation.dto.response.UploadImageWebResponse
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.nio.charset.StandardCharsets
-import javax.servlet.http.HttpServletResponse
-import javax.validation.Valid
 
 @RestController
 @RequestMapping("/application")
 class WebApplicationAdapter(
     private val createApplicationUseCase: CreateApplicationUseCase,
-    private val getInformationUseCase: GetInformationUseCase,
-    private val getIntroduceUseCase: GetIntroduceUseCase,
-    private val getStudyPlanUseCase: GetStudyPlanUseCase,
-    private val updateApplicationTypeUseCase: UpdateApplicationTypeUseCase,
-    private val updateEducationalStatusUseCase: UpdateEducationalStatusUseCase,
-    private val updateInformationUseCase: UpdateInformationUseCase,
-    private val updateIntroduceUseCase: UpdateIntroduceUseCase,
-    private val updateStudyPlanUseCase: UpdateStudyPlanUseCase,
     private val uploadPhotoUseCase: UploadPhotoUseCase,
-    private val getApplicationTypeUseCase: GetApplicationTypeUseCase,
     private val getMyApplicationStatusUseCase: GetMyApplicationStatusUseCase,
-    private val introductionPdfUseCase: GetIntroductionPdfUseCase
 ) {
     @PostMapping
     fun createApplication() {
@@ -44,107 +28,9 @@ class WebApplicationAdapter(
         )
     }
 
-    @GetMapping
-    fun getInformation(): GetInformationResponse {
-        return getInformationUseCase.execute()
-    }
-
-    @GetMapping("/intro")
-    fun getIntroduce(): GetIntroduceResponse {
-        return getIntroduceUseCase.execute()
-    }
-
-    @GetMapping("/study-plan")
-    fun getStudyPlan(): GetStudyPlanResponse {
-        return getStudyPlanUseCase.execute()
-    }
-
-    @PatchMapping("/type")
-    fun updateApplicationType(
-        @RequestBody @Valid request: UpdateApplicationTypeWebRequest,
-    ) {
-        updateApplicationTypeUseCase.execute(
-            request.run {
-                UpdateApplicationTypeRequest(
-                    applicationType = applicationType,
-                    applicationRemark = applicationRemark,
-                    isDaejeon = isDaejeon,
-                    isOutOfHeadcount = isOutOfHeadcount,
-                    veteransNumber = veteransNumber
-                )
-            },
-        )
-    }
-
-    @GetMapping("/type")
-    fun getApplicationType(): GetApplicationTypeResponse = getApplicationTypeUseCase.execute()
-
-    @PatchMapping("/graduation/type")
-    fun updateEducationalStatus(
-        @RequestBody @Valid request: UpdateEducationalStatusWebRequest,
-    ) {
-        updateEducationalStatusUseCase.execute(
-            request.run {
-                UpdateEducationalStatusRequest(
-                    educationalStatus = educationalStatus,
-                    graduateDate = graduateDate
-                )
-            },
-        )
-    }
-
-    @PatchMapping
-    fun updateInformation(
-        @RequestBody @Valid request: UpdateInformationWebRequest,
-    ) {
-        updateInformationUseCase.execute(
-            request.run {
-                UpdateInformationRequest(
-                    sex = sex,
-                    birthDate = birthDate,
-                    applicantName = applicantName,
-                    applicantTel = applicantTel,
-                    parentTel = parentTel,
-                    parentName = parentName,
-                    streetAddress = streetAddress,
-                    postalCode = postalCode,
-                    detailAddress = detailAddress,
-                    parentRelation = parentRelation
-                )
-            },
-        )
-    }
-
-    @PatchMapping("/intro")
-    fun updateIntroduce(
-        @RequestBody @Valid request: UpdateIntroduceWebRequest,
-    ) {
-        updateIntroduceUseCase.execute(
-            UpdateIntroduceRequest(request.content),
-        )
-    }
-
-    @PatchMapping("/study-plan")
-    fun updateStudyPlan(
-        @RequestBody @Valid request: UpdateStudyPlanWebRequest,
-    ) {
-        updateStudyPlanUseCase.execute(
-            UpdateStudyPlanRequest(request.content),
-        )
-    }
-
 //    @PostMapping("/final-submit")
 //    fun submitApplicationFinal() = submitApplicationFinalUseCase.execute()
 
     @GetMapping("/status")
     fun getMyApplicationStatus(): GetApplicationStatusResponse = getMyApplicationStatusUseCase.execute()
-
-    @GetMapping("/pdf/introduction", produces = [MediaType.APPLICATION_PDF_VALUE])
-    fun getIntroductionPdf(response: HttpServletResponse): ByteArray {
-        response.setHeader("Content-Disposition", "attachment; filename=\"${encodeFileName()}.pdf\"")
-        return introductionPdfUseCase.execute()
-    }
-    private fun encodeFileName(): String {
-        return String(WebApplicationPdfAdapter.FILE_NAME.toByteArray(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1)
-    }
 }
