09733978f333e11680b23f464e2ead083be02f5e
coehgns <modooboiroo@gmail.com>
2025-12-29 22:31:23 +0900

    refactor ( #74 ) : WebApplicationAdapter

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/WebApplicationAdapter.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/WebApplicationAdapter.kt
@@ -1,9 +1,10 @@
 package hs.kr.entrydsm.application.domain.application.presentation
 
+import hs.kr.entrydsm.application.domain.application.presentation.dto.request.SubmissionApplicationWebRequest
 import hs.kr.entrydsm.application.domain.application.usecase.GetMyApplicationStatusUseCase
 import hs.kr.entrydsm.application.domain.application.usecase.SubmitApplicationUseCase
 import hs.kr.entrydsm.application.domain.application.usecase.UploadPhotoUseCase
-import hs.kr.entrydsm.application.domain.application.usecase.dto.request.SubmissionApplicationWebRequest
+import hs.kr.entrydsm.application.domain.application.usecase.dto.request.SubmissionApplicationRequest
 import hs.kr.entrydsm.application.domain.application.usecase.dto.response.GetApplicationStatusResponse
 import hs.kr.entrydsm.application.domain.file.presentation.converter.ImageFileConverter
 import hs.kr.entrydsm.application.domain.file.presentation.dto.response.UploadImageWebResponse
@@ -23,14 +24,10 @@ class WebApplicationAdapter(
     private val getMyApplicationStatusUseCase: GetMyApplicationStatusUseCase,
     private val submitApplicationUseCase: SubmitApplicationUseCase
 ) {
-//    @PostMapping
-//    fun createApplication() {
-//        createApplicationUseCase.execute()
-//    }
 
     @PostMapping
     fun submission(@RequestBody @Valid request: SubmissionApplicationWebRequest) {
-        submitApplicationUseCase.execute(request)
+        submitApplicationUseCase.execute(request.toSubmissionApplicationRequest())
     }
 
     @PostMapping("/photo")
@@ -42,9 +39,59 @@ class WebApplicationAdapter(
         )
     }
 
-//    @PostMapping("/final-submit")
-//    fun submitApplicationFinal() = submitApplicationFinalUseCase.execute()
-
     @GetMapping("/status")
     fun getMyApplicationStatus(): GetApplicationStatusResponse = getMyApplicationStatusUseCase.execute()
+
+    private fun SubmissionApplicationWebRequest.toSubmissionApplicationRequest(): SubmissionApplicationRequest =
+        SubmissionApplicationRequest(
+            applicantName = applicantName,
+            applicantTel = applicantTel,
+            applicationType = applicationType,
+            educationalStatus = educationalStatus,
+            birthDate = birthDate,
+            applicantGender = applicantGender,
+            streetAddress = streetAddress,
+            postalCode = postalCode,
+            detailAddress = detailAddress,
+            isDaejeon = isDaejeon,
+            parentName = parentName,
+            parentTel = parentTel,
+            parentRelation = parentRelation,
+            guardianGender = guardianGender,
+            schoolCode = schoolCode,
+            schoolName = schoolName,
+            studentId = studentId,
+            schoolPhone = schoolPhone,
+            teacherName = teacherName,
+            nationalMeritChild = nationalMeritChild,
+            specialAdmissionTarget = specialAdmissionTarget,
+            graduationDate = graduationDate,
+            studyPlan = studyPlan,
+            selfIntroduce = selfIntroduce,
+
+            koreanGrade = koreanGrade,
+            socialGrade = socialGrade,
+            historyGrade = historyGrade,
+            mathGrade = mathGrade,
+            scienceGrade = scienceGrade,
+            englishGrade = englishGrade,
+            techAndHomeGrade = techAndHomeGrade,
+
+            gedKorean = gedKorean,
+            gedSocial = gedSocial,
+            gedHistory = gedHistory,
+            gedMath = gedMath,
+            gedScience = gedScience,
+            gedTech = gedTech,
+            gedEnglish = gedEnglish,
+
+            absence = absence,
+            tardiness = tardiness,
+            earlyLeave = earlyLeave,
+            classExit = classExit,
+            volunteer = volunteer,
+
+            algorithmAward = algorithmAward,
+            infoProcessingCert = infoProcessingCert
+        )
 }
