3f93e2b0a205918db8497ed56fc8b21b8bb85fa9
coehgns <modooboiroo@gmail.com>
2025-12-29 18:53:10 +0900

    feat ( #74 ) : ApplicationProducer에 submitApplication 구현

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/ApplicationProducer.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/ApplicationProducer.kt
@@ -1,24 +1,24 @@
 package hs.kr.entrydsm.application.domain.application.event
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import hs.kr.entrydsm.application.domain.application.event.dto.ApplicationScoreData
+import hs.kr.entrydsm.application.domain.application.event.dto.ApplicationSubmittedEvent
 import hs.kr.entrydsm.application.domain.application.event.dto.CreateApplicationEvent
+import hs.kr.entrydsm.application.domain.application.event.dto.SubmissionData
 import hs.kr.entrydsm.application.domain.application.event.spi.ApplicationEventPort
-import hs.kr.entrydsm.application.domain.application.event.dto.UpdateEducationalStatusEvent
-import hs.kr.entrydsm.application.domain.application.model.types.EducationalStatus
 import hs.kr.entrydsm.application.global.kafka.config.KafkaTopics
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
-import java.time.YearMonth
 import java.util.*
 
 @Component
 class ApplicationProducer(
     private val mapper: ObjectMapper,
     private val createApplicationTemplate: KafkaTemplate<String, Any>,
-    private val updateEducationalStatusTemplate: KafkaTemplate<String, Any>,
-    private val submitApplicationFinalTemplate: KafkaTemplate<String, Any>,
-    private val createApplicationRollbackTemplate: KafkaTemplate<String, Any>
+    private val createApplicationRollbackTemplate: KafkaTemplate<String, Any>,
+    private val publishApplicationSubmittedTemplate: KafkaTemplate<String, Any>
 ) : ApplicationEventPort {
+
     override fun create(receiptCode: Long, userId: UUID) {
         val createApplicationEvent = CreateApplicationEvent(
             userId = userId,
@@ -30,23 +30,47 @@ class ApplicationProducer(
         )
     }
 
-    override fun updateEducationalStatus(receiptCode: Long, graduateDate: YearMonth) {
-        updateEducationalStatusTemplate.send(
-            KafkaTopics.UPDATE_EDUCATIONAL_STATUS,
-            UpdateEducationalStatusEvent(receiptCode, graduateDate),
+    override fun submitApplication(submissionData: SubmissionData) {
+        val event = ApplicationSubmittedEvent(
+            receiptCode = submissionData.receiptCode,
+            userId = submissionData.userId,
+            educationalStatus = submissionData.educationalStatus,
+            graduationDate = submissionData.graduationDate,
+            schoolCode = submissionData.schoolCode,
+            teacherName = submissionData.teacherName,
+            schoolPhone = submissionData.schoolPhone,
+            scoreData = ApplicationScoreData(
+                koreanGrade = submissionData.scoreData.koreanGrade,
+                socialGrade = submissionData.scoreData.socialGrade,
+                historyGrade = submissionData.scoreData.historyGrade,
+                mathGrade = submissionData.scoreData.mathGrade,
+                scienceGrade = submissionData.scoreData.scienceGrade,
+                englishGrade = submissionData.scoreData.englishGrade,
+                techAndHomeGrade = submissionData.scoreData.techAndHomeGrade,
+                gedKorean = submissionData.scoreData.gedKorean,
+                gedSocial = submissionData.scoreData.gedSocial,
+                gedHistory = submissionData.scoreData.gedHistory,
+                gedMath = submissionData.scoreData.gedMath,
+                gedScience = submissionData.scoreData.gedScience,
+                gedEnglish = submissionData.scoreData.gedEnglish,
+                absence = submissionData.scoreData.absence,
+                tardiness = submissionData.scoreData.tardiness,
+                earlyLeave = submissionData.scoreData.earlyLeave,
+                classExit = submissionData.scoreData.classExit,
+                volunteer = submissionData.scoreData.volunteer,
+                algorithmAward = submissionData.scoreData.algorithmAward,
+                infoProcessingCert = submissionData.scoreData.infoProcessingCert
+            )
+        )
+        publishApplicationSubmittedTemplate.send(
+            KafkaTopics.SUBMIT_APPLICATION,
+            mapper.writeValueAsString(event)
         )
     }
 
     override fun createApplicationScoreRollback(receiptCode: Long) {
         createApplicationRollbackTemplate.send(
             KafkaTopics.CREATE_APPLICATION_SCORE_ROLLBACK,
-            receiptCode
-        )
-    }
-
-    override fun submitApplicationFinal(receiptCode: Long) {
-        submitApplicationFinalTemplate.send(
-            KafkaTopics.SUBMIT_APPLICATION_FINAL,
             receiptCode
         )
     }
