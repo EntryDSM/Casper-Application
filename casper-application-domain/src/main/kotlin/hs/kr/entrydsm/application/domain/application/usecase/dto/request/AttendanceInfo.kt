8f83b75bcce01aec2f57001d141bc655af7b5d58
coehgns <modooboiroo@gmail.com>
2025-12-31 22:36:56 +0900

    feat ( #74 ) : AttendanceInfo

---
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/application/usecase/dto/request/AttendanceInfo.kt
@@ -0,0 +1,9 @@
+package hs.kr.entrydsm.application.domain.application.usecase.dto.request
+
+data class AttendanceInfo(
+    val absence: Int,
+    val tardiness: Int,
+    val earlyLeave: Int,
+    val classExit: Int,
+    val volunteer: Int
+)
