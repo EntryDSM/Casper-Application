039e50683a51ecdb606564de587ce21f06c0ef35
coehgns <modooboiroo@gmail.com>
2026-01-01 16:57:53 +0900

    chore ( #74 ) : StudentNumber에 from companion object 메서드 추가

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/graduationInfo/model/vo/StudentNumber.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/graduationInfo/model/vo/StudentNumber.kt
@@ -4,4 +4,14 @@ data class StudentNumber(
     val gradeNumber: String,
     val classNumber: String,
     val studentNumber: String,
-)
+) {
+    companion object {
+        fun from(studentNumber: String): StudentNumber {
+            return StudentNumber(
+                gradeNumber = studentNumber.substring(0, 1),
+                classNumber = studentNumber.substring(1, 2),
+                studentNumber = studentNumber.substring(2)
+            )
+        }
+    }
+}
