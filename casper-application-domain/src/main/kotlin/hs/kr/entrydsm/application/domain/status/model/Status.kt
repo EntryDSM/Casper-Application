084117872c79f70aaabc409d7029871bb4fd6acc
coehgns <modooboiroo@gmail.com>
2026-01-05 21:53:54 +0900

    refactor ( #71 ) : Status applicationStatus 필드 추가

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/status/model/Status.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/status/model/Status.kt
@@ -6,10 +6,20 @@ import java.time.LocalDateTime
 @Aggregate
 data class Status(
     val id: Long? = 0,
-    val isPrintsArrived: Boolean = false,
-    val isSubmitted: Boolean = false,
+    val applicationStatus: ApplicationStatus,
+//    val isPrintsArrived: Boolean = false,
+//    val isSubmitted: Boolean = false,
     val examCode: String? = null,
     val isFirstRoundPass: Boolean = false,
     val isSecondRoundPass: Boolean = false,
     val receiptCode: Long,
-)
+) {
+    val isSubmitted: Boolean
+        get() = applicationStatus != ApplicationStatus.NOT_APPLIED &&
+                applicationStatus != ApplicationStatus.WRITING
+
+    val isPrintsArrived: Boolean
+        get() = applicationStatus == ApplicationStatus.DOCUMENTS_RECEIVED ||
+                applicationStatus == ApplicationStatus.SCREENING_IN_PROGRESS ||
+                applicationStatus == ApplicationStatus.RESULT_ANNOUNCED
+}
