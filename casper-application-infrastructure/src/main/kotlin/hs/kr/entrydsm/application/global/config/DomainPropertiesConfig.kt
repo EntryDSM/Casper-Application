7889a6901a83f5851986205aeb65e9ea9a23ee7c
coehgns <modooboiroo@gmail.com>
2026-01-06 16:23:51 +0900

    chore ( #71 ) : @ConstructorBinding 제거

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/DomainPropertiesConfig.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/config/DomainPropertiesConfig.kt
@@ -2,9 +2,7 @@ package hs.kr.entrydsm.application.global.config
 
 import hs.kr.entrydsm.application.global.DomainProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties("domain")
 class DomainPropertiesConfig(
     model: Map<String, String>,
