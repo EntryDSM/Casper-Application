cbf7ee14e4d09e0488cc3e000fe5803e2fc51ea8
coehgns <modooboiroo@gmail.com>
2026-01-06 20:21:25 +0900

    chore ( #71 ) : ScheduleInfoElement 제거

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/feign/client/dto/response/ScheduleInfoElement.kt
+++
@@ -1,9 +0,0 @@
-package hs.kr.entrydsm.application.global.feign.client.dto.response
-
-import hs.kr.entrydsm.application.domain.schedule.enums.ScheduleType
-import java.time.LocalDateTime
-
-data class ScheduleInfoElement (
-    val type: ScheduleType,
-    val date: LocalDateTime
-)
