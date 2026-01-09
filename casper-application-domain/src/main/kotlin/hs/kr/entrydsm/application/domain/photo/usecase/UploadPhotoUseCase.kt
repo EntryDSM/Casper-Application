b5529d3dadc29b0d0c8c7676c932024ed20bd948
coehgns <modooboiroo@gmail.com>
2026-01-09 16:57:46 +0900

    feat ( #90 ) : UploadPhotoUseCase

---
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/photo/usecase/UploadPhotoUseCase.kt
@@ -0,0 +1,36 @@
+package hs.kr.entrydsm.application.domain.photo.usecase
+
+import hs.kr.entrydsm.application.domain.file.spi.UploadFilePort
+import hs.kr.entrydsm.application.domain.file.usecase.`object`.PathList
+import hs.kr.entrydsm.application.domain.photo.model.Photo
+import hs.kr.entrydsm.application.domain.photo.spi.CommandPhotoPort
+import hs.kr.entrydsm.application.domain.photo.spi.QueryPhotoPort
+import hs.kr.entrydsm.application.global.annotation.UseCase
+import hs.kr.entrydsm.application.global.security.spi.SecurityPort
+import java.io.File
+import java.util.UUID
+
+@UseCase
+class UploadPhotoUseCase(
+    private val securityPort: SecurityPort,
+    private val queryPhotoPort: QueryPhotoPort,
+    private val commandPhotoPort: CommandPhotoPort,
+    private val uploadFilePort: UploadFilePort,
+) {
+    fun execute(file: File): String {
+        val userId = UUID.fromString("c5e99b01-561b-45f5-9ae3-78c76c1670ba")
+        val photo = uploadFilePort.upload(file, PathList.PHOTO)
+
+        queryPhotoPort.queryPhotoByUserId(userId)?.apply {
+            this.photoPath = photo
+            commandPhotoPort.save(this)
+        } ?: commandPhotoPort.save(
+            Photo(
+                userId = userId,
+                photoPath = photo,
+            ),
+        )
+
+        return photo
+    }
+}
