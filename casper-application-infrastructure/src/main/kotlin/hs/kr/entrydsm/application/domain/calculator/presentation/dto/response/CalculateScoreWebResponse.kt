33d6591cecf1764f47f92f9d636d2f1aa0efe8be
coehgns <modooboiroo@gmail.com>
2026-01-13 23:06:54 +0900

    feat ( #103 ) : CalculateScoreWebResponse

---
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/calculator/presentation/dto/response/CalculateScoreWebResponse.kt
@@ -0,0 +1,7 @@
+package hs.kr.entrydsm.application.domain.calculator.presentation.dto.response
+
+import java.math.BigDecimal
+
+data class CalculateScoreWebResponse(
+    val totalScore: BigDecimal
+)
