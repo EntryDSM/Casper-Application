c0855fdaff30e336ee869ef322de86a752ad8b49
coehgns <modooboiroo@gmail.com>
2026-01-14 16:04:03 +0900

    chore ( #92 ) : 미사용 로직 제거

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/kafka/config/KafkaTopics.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/global/kafka/config/KafkaTopics.kt
@@ -2,12 +2,12 @@ package hs.kr.entrydsm.application.global.kafka.config
 
 object KafkaTopics {
     const val CREATE_APPLICATION = "create-application"
-    const val UPDATE_EDUCATIONAL_STATUS = "update-educational-status"
-    const val UPDATE_GRADUATION_CASE = "update-graduation-case"
-    const val UPDATE_QUALIFICATION_CASE = "update-qualification-case"
-    const val UPDATE_APPLICATION_CASE_ROLLBACK = "update-application-case-rollback"
-    const val DELETE_USER = "delete-user"
-    const val CREATE_APPLICATION_SCORE_ROLLBACK = "create-application-score-rollback"
     const val CREATE_APPLICATION_STATUS_ROLLBACK = "create-application-status-rollback"
     const val CANCEL_SUBMITTED_APPLICATION = "cancel-submitted-application"
+    const val DELETE_USER = "delete-user"
+//    const val UPDATE_EDUCATIONAL_STATUS = "update-educational-status"
+//    const val UPDATE_GRADUATION_CASE = "update-graduation-case"
+//    const val UPDATE_QUALIFICATION_CASE = "update-qualification-case"
+//    const val UPDATE_APPLICATION_CASE_ROLLBACK = "update-application-case-rollback"
+//    const val CREATE_APPLICATION_SCORE_ROLLBACK = "create-application-score-rollback"
 }
