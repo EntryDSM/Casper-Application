8f31c0d64ada251dc491cbd90626bdcd7dd80c4a
coehgns <modooboiroo@gmail.com>
2026-01-14 14:30:23 +0900

    refactor ( #105 ) : QualificationMapper

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/graduationInfo/domain/mapper/QualificationMapper.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/graduationInfo/domain/mapper/QualificationMapper.kt
@@ -3,13 +3,31 @@ package hs.kr.entrydsm.application.domain.graduationInfo.domain.mapper
 import hs.kr.entrydsm.application.domain.graduationInfo.domain.entity.QualificationJpaEntity
 import hs.kr.entrydsm.application.domain.graduationInfo.model.Qualification
 import hs.kr.entrydsm.application.global.mapper.GenericMapper
-import org.mapstruct.Mapper
+import org.springframework.stereotype.Component
 
-@Mapper
-abstract class QualificationMapper : GenericMapper<QualificationJpaEntity, Qualification> {
-    abstract override fun toEntity(model: Qualification): QualificationJpaEntity
+@Component
+class QualificationMapper : GenericMapper<QualificationJpaEntity, Qualification> {
+    override fun toEntity(model: Qualification): QualificationJpaEntity {
+        return QualificationJpaEntity(
+            id = model.id,
+            isProspectiveGraduate = model.isProspectiveGraduate,
+            receiptCode = model.receiptCode,
+            graduateDate = model.graduateDate
+        )
+    }
 
-    abstract override fun toDomain(entity: QualificationJpaEntity?): Qualification?
+    override fun toDomain(entity: QualificationJpaEntity?): Qualification? {
+        return entity?.let {
+            Qualification(
+                id = it.id,
+                isProspectiveGraduate = it.isProspectiveGraduate,
+                receiptCode = it.receiptCode,
+                graduateDate = it.graduateDate
+            )
+        }
+    }
 
-    abstract override fun toDomainNotNull(entity: QualificationJpaEntity): Qualification
+    override fun toDomainNotNull(entity: QualificationJpaEntity): Qualification {
+        return toDomain(entity)!!
+    }
 }
