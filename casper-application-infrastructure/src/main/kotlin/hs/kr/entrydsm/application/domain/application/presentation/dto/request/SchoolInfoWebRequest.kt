0709df81f4166d7abbc3162eed55503717aad45d
coehgns <modooboiroo@gmail.com>
2026-01-14 09:36:18 +0900

    refactor ( #105 ) : SchoolInfoWebRequest

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/dto/request/SchoolInfoWebRequest.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/presentation/dto/request/SchoolInfoWebRequest.kt
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.NotBlank
 
 data class SchoolInfoWebRequest(
     @field:NotBlank(message = "학교 코드는 필수입니다")
-    val schoolCode: String,
+    val schoolCode: String?,
     @field:NotBlank(message = "학교명은 필수입니다")
-    val schoolName: String,
+    val schoolName: String?,
     @field:NotBlank(message = "학교 전화번호는 필수입니다")
-    val schoolPhone: String,
+    val schoolPhone: String?,
     @field:NotBlank(message = "담임 교사 이름은 필수입니다")
-    val teacherName: String,
+    val teacherName: String?,
 )
