84e368338beda1be50deb6e1796dd0db50217ef1
coehgns <modooboiroo@gmail.com>
2026-01-18 23:01:22 +0900

    refactor ( #92 ) : ApplicationEventListener

--- casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/ApplicationEventListener.kt
+++ casper-application-infrastructure/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/ApplicationEventListener.kt
@@ -2,6 +2,7 @@ package hs.kr.entrydsm.application.domain.application.event
 
 import hs.kr.entrydsm.application.domain.application.event.model.ApplicationSubmittedEvent
 import hs.kr.entrydsm.application.domain.application.event.spi.ApplicationEventPort
+import hs.kr.entrydsm.application.domain.saga.service.ApplicationSagaService
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -9,10 +10,12 @@ import org.springframework.transaction.event.TransactionalEventListener
 @Component
 class ApplicationEventListener(
     private val applicationEventPort: ApplicationEventPort,
+    private val applicationSagaService: ApplicationSagaService,
 ) {
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun sendApplicationCreateCompleted(event: ApplicationSubmittedEvent) {
+        applicationSagaService.initialize(event.receiptCode)
         applicationEventPort.sendApplicationCreateCompleted(event.receiptCode, event.userId)
     }
 }
