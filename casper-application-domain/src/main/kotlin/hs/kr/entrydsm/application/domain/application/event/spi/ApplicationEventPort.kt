9cd19cdf6078b0bce0fe721499c68613df896358
coehgns <modooboiroo@gmail.com>
2026-01-18 22:20:55 +0900

    refactor ( #92 ) : ApplicationEventPort

--- casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/spi/ApplicationEventPort.kt
+++ casper-application-domain/src/main/kotlin/hs/kr/entrydsm/application/domain/application/event/spi/ApplicationEventPort.kt
@@ -3,7 +3,7 @@ package hs.kr.entrydsm.application.domain.application.event.spi
 import java.util.UUID
 
 interface ApplicationEventPort {
-    fun create(receiptCode: Long, userId: UUID)
+    fun sendApplicationCreateCompleted(receiptCode: Long, userId: UUID)
 
     fun cancelSubmittedApplication(receiptCode: Long)
 }
